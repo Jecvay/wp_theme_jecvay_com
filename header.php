fed6b156dc58abe179dc4db8cc8e03a19db5e725
Jecvay <ljwjiewei@gmail.com>
2021-03-11 01:44:55 +0800

    hugo的移植过来

--- header.php
+++ header.php
@@ -23,7 +23,8 @@
 			})();
 		</script>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri()."/static/css/style.css";?>">
-    <link rel=stylesheet href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,700;1,400;1,700&family=Noto+Serif+SC:wght@400;500;700&display=swap">
+    <link rel=preconnect href=https://fonts.gstatic.com crossorigin><link rel=stylesheet href="https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;700&family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Cinzel+Decorative:wght@700&display=swap" media=print onload="this.media='all'">
+	<noscript><link rel=stylesheet href="https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;700&family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Cinzel+Decorative:wght@700&display=swap"></noscript>
 	</head>
 
   <body>
