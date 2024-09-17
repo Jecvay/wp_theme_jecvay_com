11964c4c07bf0ecd86359575e025eaea52ab3776
Jecvay <ljwjiewei@gmail.com>
2024-09-17 19:21:53 +0800

    footer

--- footer.php
+++ footer.php
@@ -7,11 +7,8 @@
 
 
   <footer id="footer">
-    <p class="small">
-      © Copyright 2021 <i class="fa fa-heart" aria-hidden="true"></i> Jecvay &nbsp&nbsp
-        <a href="http://beian.miit.gov.cn/" style="color:#bbb">粤ICP备2021157980号</a>
-    </p>
-    <div class="copyright"> &nbsp</div>
+    <p class="small">© Copyright <i class="fa fa-heart" aria-hidden="true"></i>Jecvay</p>
+    <div class="copyright"></div>
   </footer>
   <?php wp_footer();?>
   <script src="<?php echo get_template_directory_uri()."/static/js/jquery-3.3.1.min.js";?>"></script>
